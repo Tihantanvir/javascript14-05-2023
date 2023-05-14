d0996698570c9960af37e856fcf4c51ca6f7b5c1
Tihantanvir <tihantanvir@gmail.com>
2023-05-14 09:01:56 +0600

    first commit

---
+++ qureyselectorall.js
@@ -0,0 +1,9 @@
+// hey world how 
+//  you just bloging 
+//  here about this
+//  are you im fine thank 
+//  you im just here abou
+
+
+
+// 698960
---
+++ tour-of-dom.js
@@ -0,0 +1,20 @@
+// document.getElementsByClassName('header')
+// console.log('header');
+// console.log(document); 
+
+// const allHeadlings = document.getElementsByTagName('h1');
+// for (const h1 of allHeadlings){
+// console.log(h1.innerText);
+// }
+
+//  const id = document.getElementByid('sections-two');
+//  for (const 'sections-two' of id){
+ 
+//  console.log( li.innerText);
+//  }
+document.getElementsByClassName('li');
+console.log('li');
+
+
+// peace all ways wrong this 
+// peace i worng just to here about this 
