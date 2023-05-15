a9ecc84579aab6bbfabca17aa52a1660b4542109
Tihantanvir <tihantanvir@gmail.com>
2023-05-15 21:37:50 +0600

    js dom

--- tour-of-dom.js
+++ tour-of-dom.js
@@ -12,9 +12,148 @@
  
 //  console.log( li.innerText);
 //  }
-document.getElementsByClassName('li');
-console.log('li');
+// document.getElementsByClassName('li');
+// console.log('li');
 
 
 // peace all ways wrong this 
 // peace i worng just to here about this 
+
+// var firstname = 'tihan';
+// var lastname = 'tanvir';
+// var fullname = firstname + ' TANVIR ' + lastname ;
+// console.log(fullname);
+
+// var hello  = 'tanvir' ;
+// var how = 'im just';
+// var helloHowyou = hello + 'Hey baby im just here about this ' + how ;
+// console.log(helloHowyou);
+
+
+
+// number type variable 
+
+// var tanvir = 223 ;
+// string type of variable
+
+// var tanvir = 'string';
+
+// boolean type variable
+
+// var tanvir = true ;
+
+// var tanvir = false ;
+
+// tow type of number  
+
+// intiger purnhoshonka var tanvir = 120 ;
+
+// firstfloat decimal mane dhoshomik shonka var tanvir = 3.65 ;
+//  variable naming conventions rules
+
+// 1. single word  use 
+
+// 2 .no qoute use 
+
+// 3. no keyword use
+
+// 4. not number start 
+
+// 5 . should use camelCase everytime use
+
+// math operation + , -, *, /   
+
+// +=, -=, --, == 
+
+
+
+
+// convert intiger to firstFloat
+
+// riminder % vagshes
+
+
+// var friendAge = [39, 46, 34, 53, 70] ;
+
+// var picnicFee = [300, 400, 500, 300, 500, 700];
+
+// var naikays = ['porimoni', 'purnima', 'nusrat'];
+
+// var viowels = ['a', 'e', 'i', 'o', 'u'];
+
+// console.log(picnicFee[2]);
+
+
+
+
+// get element value by index 
+
+// var numbers = [12, 23, 42, 67, 78, 90, 50];
+
+// console.log(numbers[3]);
+
+
+
+// set element value by index 
+
+// numbers[6] = 50000 ;
+
+// console.log(numbers);
+
+
+
+
+// find elments value from indexof
+
+// var numbers = [12, 23, 42, 67, 78, 90, 50];
+
+// console.log[numbers.indexOf(42)];
+
+                    //   18. 4
+
+// var numberss = [50, 82, 42, 60, 75, 60, 51];
+// numberss.push(4000);
+// console.log(numberss);
+
+
+// var tanvirRule = ['web developer', 'react developer', 'hey world im jsut here'];
+// tanvirRule.push('baby developer');
+// console.log(tanvirRule);
+
+
+// var baby = [1, 2, 3, 4, 5, 6, 7, 8, 9, 200];
+// baby.push(50000);
+// console.log(baby);
+
+
+// var tanvir = [25, 50, 544,500];
+// tanvir.pop();
+// console.log(tanvir);
+
+
+// var tanvirRule = true ;
+
+// tanvirRule.push();
+
+// console.log(tanvirRule);
+
+
+
+// var kalughaithte_murir_tin = ['hello', 'world', 'how']; 
+// kalughaithte_murir_tin.push('helloooooooo');
+// console.log(kalughaithte_murir_tin);
+
+
+
+
+// var gigabyte = ['hello', 'how'];
+// console.log(gigabyte);
+
+// var tihan = [455, 48858, 4848, 8448, 8458540];
+
+// console.log(tihan);
+ 
+
+                     // 18. 5 
+
+// go to practice here about thi
